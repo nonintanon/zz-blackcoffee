0d6561e82dd12387ed481205bb08758be356e427
nonintanon <non@mrnon.com>
2013-09-15 14:20:56 +0700

    add mvc project

---
+++ ZZBlackCoffeeAzureAndGitDemo/App_Start/BundleConfig.cs
@@ -0,0 +1,60 @@
+﻿using System.Web;
+using System.Web.Optimization;
+
+namespace ZZBlackCoffeeAzureAndGitDemo
+{
+    public class BundleConfig
+    {
+        // For more information on Bundling, visit http://go.microsoft.com/fwlink/?LinkId=254725
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            //bundles.UseCdn = true;
+
+            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+                        "~/Scripts/jquery-{version}.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/jqueryui").Include(
+                        "~/Scripts/jquery-ui-{version}.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
+                        "~/Scripts/jquery.unobtrusive*",
+                        "~/Scripts/jquery.validate*"));
+
+            // Use the development version of Modernizr to develop with and learn from. Then, when you're
+            // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
+            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
+                        "~/Scripts/modernizr-*"));
+
+            bundles.Add(new ScriptBundle("~/bundles/libs").Include(
+                        "~/Scripts/bootstrap.js", // don't bundle the min version, it will be broken
+                        "~/Scripts/knockout-*"));
+            //"~/Scrtips/jquery.isotope.js", // don't bundle the min version, it will be broken
+            //"~/Scripts/jquery.magnific-popup.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/main").Include("~/Scripts/main.js"));
+
+            bundles.Add(new StyleBundle("~/styles").Include(
+                //"~/Content/bootstrap.css", 
+                //"~/Content/bootstrap-themes.css",
+                        "~/Content/font-awesome.css",
+                        "~/Content/font-awesome-ie7.css",
+                        "~/Content/css/style.css"));
+
+            bundles.Add(new StyleBundle("~/Content/themes/base/css").Include(
+                        "~/Content/themes/base/jquery.ui.core.css",
+                        "~/Content/themes/base/jquery.ui.resizable.css",
+                        "~/Content/themes/base/jquery.ui.selectable.css",
+                        "~/Content/themes/base/jquery.ui.accordion.css",
+                        "~/Content/themes/base/jquery.ui.autocomplete.css",
+                        "~/Content/themes/base/jquery.ui.button.css",
+                        "~/Content/themes/base/jquery.ui.dialog.css",
+                        "~/Content/themes/base/jquery.ui.slider.css",
+                        "~/Content/themes/base/jquery.ui.tabs.css",
+                        "~/Content/themes/base/jquery.ui.datepicker.css",
+                        "~/Content/themes/base/jquery.ui.progressbar.css",
+                        "~/Content/themes/base/jquery.ui.theme.css"));
+
+            //BundleTable.EnableOptimizations = true;
+        }
+    }
+}
---
+++ ZZBlackCoffeeAzureAndGitDemo/Scripts/main.js
@@ -0,0 +1,119 @@
+﻿function ViewModel() {
+    this.players = [
+        { position: "Forward", number: "21", image: "/images/21.jpg", name: "Angelo Henriquez" },
+        { position: "Goalkeeper", number: "01", image: "/images/01.jpg", name: "David de Gea" },
+        { position: "Goalkeeper", number: "40", image: "/images/40.jpg", name: "Ben Amos" },
+        { position: "Defender", number: "03", image: "/images/03.jpg", name: "Patrice Evra" },
+        { position: "Midfielder", number: "08", image: "/images/08.jpg", name: "Anderson" },
+        { position: "Midfielder", number: "33", image: "/images/33.jpg", name: "Bebe" },
+        { position: "Defender", number: "28", image: "/images/28.jpg", name: "Alexander Butner" },
+        { position: "Defender", number: "30", image: "/images/30.jpg", name: "Guillermo Varela" },
+        { position: "Midfielder", number: "31", image: "/images/31.jpg", name: "Marouane Fellaini" },
+        { position: "Midfielder", number: "16", image: "/images/16.jpg", name: "Michael Carrick" },
+        { position: "Midfielder", number: "26", image: "/images/26.jpg", name: "Shinji Kagawa" },
+        { position: "Midfielder", number: "32", image: "/images/32.jpg", name: "Nick Powell" },
+        { position: "Midfielder", number: "23", image: "/images/23.jpg", name: "Tom Cleverley" },
+        { position: "Defender", number: "06", image: "/images/06.jpg", name: "Jonny Evans" },
+        { position: "Defender", number: "22", image: "/images/22.jpg", name: "Fabio" },
+        { position: "Defender", number: "05", image: "/images/05.jpg", name: "Rio Ferdinand" },
+        { position: "Midfielder", number: "24", image: "/images/24.jpg", name: "Darren Fletcher" },
+        { position: "Midfielder", number: "11", image: "/images/11.jpg", name: "Ryan Giggs" },
+        { position: "Midfielder", number: "34", image: "/images/34.jpg", name: "Larnell Cole" },
+        { position: "Goalkeeper", number: "13", image: "/images/13.jpg", name: "Anders Lindegaard" },
+        { position: "Forward", number: "14", image: "/images/14.jpg", name: "Javier Hernandez" },
+        { position: "Midfielder", number: "35", image: "/images/35.jpg", name: "Jesse Lingard" },
+        { position: "Defender", number: "38", image: "/images/38.jpg", name: "Michael Keane" },
+        { position: "Goalkeeper", number: "50", image: "/images/50.jpg", name: "Sam Johnstone" },
+        { position: "Forward", number: "27", image: "/images/27.jpg", name: "Federico Macheda" },
+        { position: "Midfielder", number: "44", image: "/images/44.jpg", name: "Adnan Januzaj" },
+        { position: "Midfielder", number: "17", image: "/images/17.jpg", name: "Nani" },
+        { position: "Midfielder", number: "29", image: "/images/29.jpg", name: "Wilfried Zaha" },
+        { position: "Defender", number: "02", image: "/images/02.jpg", name: "Rafael" },
+        { position: "Forward", number: "10", image: "/images/10.jpg", name: "Wayne Rooney" },
+        { position: "Midfielder", number: "18", image: "/images/18.jpg", name: "Ashley Young" },
+        { position: "Defender", number: "12", image: "/images/12.jpg", name: "Chris Smalling" },
+        { position: "Defender", number: "04", image: "/images/04.jpg", name: "Phil Jones" },
+        { position: "Midfielder", number: "25", image: "/images/25.jpg", name: "Antonio Valencia" },
+        { position: "Forward", number: "20", image: "/images/20.jpg", name: "Robin van Persie" },
+        { position: "Defender", number: "15", image: "/images/15.jpg", name: "Nemanja Vidic" },
+        { position: "Forward", number: "19", image: "/images/19.jpg", name: "Danny Welbeck" }
+    ];
+
+    this.selectedProducts = ko.observable();
+    this.resetSelectedProducts = function () { this.selectedProducts(null); }
+}
+
+ko.applyBindings(new ViewModel());
+
+$(document).ready(function () {
+
+    var $container = $('.products');
+    $container.isotope({
+        itemSelector: '.product',
+        layoutMode: 'masonry',
+        getSortData: {
+            category: function ($elem) { return $elem.attr('data-category'); },
+            name: function ($elem) { return $elem.find('.name').text(); },
+            sku: function ($elem) { return $elem.find('.sku').text(); }
+        }
+    }, function ($items) {
+        var id = this.attr('id');
+        var len = $items.length;
+        console.log('Isotope has filtered for ' + len + ' items in #' + id);
+    });
+    $('#player-sorting a').click(function () {
+        var sortName = $(this).attr('href').slice(1);
+        console.log(sortName);
+        $container.isotope({ sortBy: sortName, sortAscending: true });
+        return false;
+    });
+    $('#player-filtering a').on('click', function () {
+        var selector = $(this).attr('data-filter');
+        $container.isotope({ filter: selector });
+        return false;
+    });
+
+    // Inline popups
+    $('#inline-popups').magnificPopup({
+        delegate: 'a',
+        removalDelay: 500, //delay removal by X to allow out-animation
+        callbacks: {
+            beforeOpen: function () {
+                this.st.mainClass = this.st.el.attr('data-effect');
+            }
+        },
+        midClick: true // allow opening popup on middle mouse click. Always set it to true if you don't provide alternative source.
+    });
+
+    // Image popups
+    $('.image-link').magnificPopup({
+        //delegate: 'a',
+        type: 'image',
+        removalDelay: 500, //delay removal by X to allow out-animation
+        callbacks: {
+            beforeOpen: function () {
+                // just a hack that adds mfp-anim class to markup 
+                this.st.image.markup = this.st.image.markup.replace('mfp-figure', 'mfp-figure mfp-with-anim');
+                this.st.mainClass = 'mfp-3d-unfold'; //this.st.el.attr('data-effect');
+            }
+        },
+        closeOnContentClick: true,
+        midClick: true // allow opening popup on middle mouse click. Always set it to true if you don't provide alternative source.
+    });
+
+
+    // Hinge effect popup
+    $('a.hinge').magnificPopup({
+        mainClass: 'mfp-with-fade',
+        removalDelay: 1000, //delay removal by X to allow out-animation
+        callbacks: {
+            beforeClose: function () {
+                this.content.addClass('hinge');
+            },
+            close: function () {
+                this.content.removeClass('hinge');
+            }
+        },
+        midClick: true
+    });
+});
